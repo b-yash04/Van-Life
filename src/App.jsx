1313eb1818b6caf58b57239ce24f8324f2f115bd
yash bhatt <bhatty048@gmail.com>
2025-07-21 14:26:56 +0530

    feat : add web pages and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
-
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header  from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
 function App() {
-  
-
   return (
-<></>
+    <>
+      <Header />
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/about" element = {<About/>}/>
+      </Routes>
+      <Footer />
+    </>
+    
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import {Routes,Route,Link} from "react-router-dom"
+export default function Header(){
+    return (
+        <header>
+           <Link to="/" className="link"> <h1 className="title"> Van Life</h1></Link> 
+            <nav>
+                <Link className="link" to="/about">About</Link>
+                <Link className="link" to="/vans">Vans</Link>
+            </nav>
+        </header>
+       
+    )
+}
