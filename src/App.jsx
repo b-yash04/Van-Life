f3d1902666aef0714f63c1274d1e2258a39ee3d6
yash bhatt <bhatty048@gmail.com>
2025-07-23 04:37:58 +0530

    Refactor the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Header  from "./components/Header.jsx"
-import Footer from "./components/Footer.jsx"
+import { Routes, Route} from "react-router-dom"
+import Layout from "./components/Layout.jsx"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import VanList from "./pages/VanList.jsx"
 import IndividualVan from "./pages/IndividualVan.jsx"
+import Dashboard from "./Host/Dashboard.jsx"
 function App() {
   return (
     <>
-      <Header />
+     
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/about" element = {<About/>}/>
-        <Route path = "/VanList" element = {<VanList/>}/>
-        <Route path = "/VanList/:id" element = {<IndividualVan/>}/>
+        <Route element = {<Layout/>}>
+          <Route path = "/" element = {<Home/>}/>
+          <Route path = "/about" element = {<About/>}/>
+          <Route path = "/VanList" element = {<VanList/>}/>
+          <Route path = "/VanList/:id" element = {<IndividualVan/>}/>
+          <Route path = "/host" element = {<Dashboard/>}/>
+        </Route>
       </Routes>
-      <Footer />
+  
     </>
     
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header(){
         <header>
            <Link to="/" className="link"> <h1 className="title"> Van Life</h1></Link> 
             <nav>
+                <Link className="link" to="/host">Host</Link>
                 <Link className="link" to="/about">About</Link>
                 <Link className="link" to="/VanList">Vans</Link>
             </nav>
